3372a9f1b17f1d52b7ae128027a5d0deda1116b6
Arsen Smerechuk <71315641+arersen@users.noreply.github.com>
2020-12-11 12:40:20 +0200

    Create calc.f95

---
+++ calc.f95
@@ -0,0 +1,25 @@
+program calc
+real a,b,c
+print,*'Input a'
+read a
+print,*'Input b'
+read b
+character oper
+read oper
+select case (operation)
+case ('-')
+print,*'a-b'
+case ('+')
+print,*'a+b'
+case ('*')
+print,*'a-b'
+case ('/')
+if (b.eq.0)then
+print,*'Delutb Nelb39'
+print,*'Input another b'
+read b
+case ('**')
+print,*'BBedite a'
+read a
+print,*'BBedite ctepenb'
+read c
